58ec2384b33b9de527c104eca6e3e54b5158f02c
Oğuz Karan <oguzkaran@csystem.org>
2022-12-03 20:46:40 +0300

    Update

---
+++ src/src-console/csd/App.java
@@ -0,0 +1,13 @@
+/*----------------------------------------------------------------------------------------------------------------------
+	Java'da yorum satırları (comment lines)
+	Anahtar Notlar: Yorum satırları kodun okunabilirliğini/algılanabilirliğini bozacak şekilde koda eklenmemelidir.
+	Yorum satırı eklemenin da bir "convention"'ı olmalıdır ve mutlaka olur.  
+-----------------------------------------------------------------------------------------------------------------------*/
+package csd;
+
+class App {
+	public static void main(String [] args) 
+	{	
+		int a;
+	}
+}
