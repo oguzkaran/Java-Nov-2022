274f10ccf7c345af14ef7cfa5bf18843a2df3d94
Oğuz Karan <oguzkaran@csystem.org>
2022-12-17 20:48:45 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,16 +1,37 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Anahtar Notlar: printf metodunun format karakterleri ve kullanım biçimleri oldukça geniştir. Burada sadece temel
-	olanlar ele alınmıştır. Diğer format karakterleri ve kullanım detayları konular içerisinde ele alınacaktır
+	Sınıf Çalışması: Parametresi ile aldığı double türden x1, y1, x2, y2 koordinat bilgilerine göre iki nokta arasındaki
+	((x1, y1) ve (x2, y2) noktaları) Euclid uzaklığına geri dönen distance isimli metodu PointUtil isimli sınıf 
+	içerisinde yazını ve aşağıdaki kod ile test ediniz 
+	
+	Not: İleride daha iyisi yazılacaktır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
-	{	
+	{		
+		DistanceTest.run();
+	}
+}
+
+class DistanceTest {
+	public static void run()
+	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("İndirim oranını giriniz:");
-		double ratio = kb.nextDouble();
+		System.out.print("Koordinatları giriniz:");	
+		double x1 = kb.nextDouble();
+		double y1 = kb.nextDouble();
+		double x2 = kb.nextDouble();
+		double y2 = kb.nextDouble();
 		
-		System.out.printf("Oran: %%%f%n", ratio);
+		System.out.printf("Distance:%f%n", PointUtil.distance(x1, y1, x2, y2));		
+		System.out.println("Tekrar yapıyor musunuz?");
+	}
+}
+
+class PointUtil {
+	public static double distance(double x1, double y1, double x2, double y2)
+	{		
+		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 	}
 }
