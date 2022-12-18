a291aaac2bfa07d1f58714df005b1a7fde8b4daa
Oğuz Karan <oguzkaran@csystem.org>
2022-12-18 18:28:15 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,37 +1,14 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı double türden x1, y1, x2, y2 koordinat bilgilerine göre iki nokta arasındaki
-	((x1, y1) ve (x2, y2) noktaları) Euclid uzaklığına geri dönen distance isimli metodu PointUtil isimli sınıf 
-	içerisinde yazını ve aşağıdaki kod ile test ediniz 
-	
-	Not: İleride daha iyisi yazılacaktır
+	Aşağıdaki örnekte iki tane karakter yazımı dolayısıyla error oluşur	
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
 	{		
-		DistanceTest.run();
-	}
-}
-
-class DistanceTest {
-	public static void run()
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Koordinatları giriniz:");	
-		double x1 = kb.nextDouble();
-		double y1 = kb.nextDouble();
-		double x2 = kb.nextDouble();
-		double y2 = kb.nextDouble();
+		char c = '\n';
 		
-		System.out.printf("Distance:%f%n", PointUtil.distance(x1, y1, x2, y2));		
-		System.out.println("Tekrar yapıyor musunuz?");
-	}
-}
-
-class PointUtil {
-	public static double distance(double x1, double y1, double x2, double y2)
-	{		
-		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+		
+		
 	}
 }
