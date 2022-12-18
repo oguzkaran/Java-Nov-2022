a5ae6864873ca8d249e50d697933d60e75b6b16b
Oğuz Karan <oguzkaran@csystem.org>
2022-12-18 20:38:14 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,14 +1,22 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Aşağıdaki örnekte iki tane karakter yazımı dolayısıyla error oluşur	
+ 	Yalnızca sabitlerden ve operatörlerden oluşan yani içerisinde değişken olmayan ifadelere "sabit ifadesi (constant expression)"
+ 	denir. Bu anlamda tek başına bir sabit de sabit ifadesidir.
+ 	
+ 	Anahtar Notlar: İleride aslında sabit ifadesi olarak kullanılabilen değişkenler de göreceğiz. Bu tarz değişkenler de 
+ 	bir ifade içerisinde sabit olarak ele alınırlar
+ 	
+ 	Derleyiciler sabit ifadelerinin değerlerini hesaplayarak sonucu "byte code"'a yazarlar. Bu optimizasyona 
+ 	"constant folding optimization" denir. Bu sebeple aşağıdaki örnekteki çarpma sonucu arakoda yazılır. Örnekteki
+ 	bazı detaylar ileride ele alınacaktır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
-	{		
-		char c = '\n';
+	{			
+		long divider = 1000L * 60 * 60 * 24 * 365; //byte code -> 31536000000
 		
-		
-		
+		System.out.println(divider);
 	}
 }
+
