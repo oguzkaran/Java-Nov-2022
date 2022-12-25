b361d96df04c74346c2399cf8ed86985f2fd62ab
Oğuz Karan <oguzkaran@csystem.org>
2022-12-25 20:46:54 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,28 +1,61 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	if deyiminde bileşk deyim kullanımı
+	Sınıf Çalışması: Katsayıları klavyeden alınan ikinci dereceden bir denklemin köklerini bulup ekrana yazdıran
+	programı yazınz.
+	Not: İleride daha iyisi yazılacaktır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
 	{		
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int val = kb.nextInt();
-		
-		if (val % 2 == 0) {
-			System.out.println("Çift sayı girdiniz");
-			val /= 2;
-		}
-		else {
-			System.out.println("Tek sayı girdiniz");
-			val *= 2;
-		}	
-		
-		System.out.printf("val = %d%n", val);
-		
-		System.out.println("Tekrar yapıyor musunuz?");
+		QuadraticEquationSolverApp.run();
 	}
 }
 
+
+class QuadraticEquationSolverApp {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("Katsayıları giriniz:");
+		double a = kb.nextDouble();
+		double b = kb.nextDouble();
+		double c = kb.nextDouble();
+
+		QuadraticEquationSolver.printRoots(a, b, c);
+	}
+}
+
+class QuadraticEquationSolver {
+	public static void doWorkForRoots(double delta, double a, double b)
+	{
+		double x1, x2;
+		double sqrtDelta;
+		
+		sqrtDelta = Math.sqrt(delta);
+		
+		x1 = (-b + sqrtDelta) / (2 * a);
+		x2 = (-b - sqrtDelta) / (2 * a);
+		
+		System.out.printf("x1 = %f, x2 = %f%n", x1, x2);
+	}
+	
+
+	public static double getDelta(double a, double b, double c)
+	{
+		return b * b - 4 * a * c;
+	}
+	
+	public static void printRoots(double a, double b, double c)
+	{
+		double delta = getDelta(a, b, c);
+		
+		if (delta >= 0)
+			doWorkForRoots(delta, a, b);
+		else
+			System.out.println("Gerçek kök yok");
+		
+	}
+}
 
