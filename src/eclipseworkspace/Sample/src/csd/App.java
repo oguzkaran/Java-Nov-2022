65cbb110d3adccfb0de5acb0c6cdfb87f9f08800
Oğuz Karan <oguzkaran@csystem.org>
2023-01-14 19:17:18 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,29 +1,42 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	continue deyimi döngüyü değil, döngünün o anki adımını sonlandırmak için kullanılır. continu deyiminin de etiketli
-	biçimi vardır. Ancak yapısal programlama ve nesne yönelimli porogramlama tekniği açısından kullanımı tavsiye edilmez.
-	continue deyiminin etiketsiz biçimi break deyimi kadar sık kullanılmaz. Ancak bazı durumlarda okunabililiği artırabilir.
-	continue deyimi yalnızca döngü içerisinde kullanılabilir. 
-	
-	Aşağıdkai örnekte 1 ile klavyeden girilen sayı arasındaki çift sayılar ekrana bastırılmıştır. Şüphesiz daha kolay
-	yazılabilir. Sadece continue deyiminin çalışma prensibinin anlaşılması için bu şekilde yazılmıştır
+	Sınıf Çalışması: Parametresi ile aldığı n sayısı için n-inci Fibonacci sayısını döndüren fibonacciNumber isimli
+	metodu NumberUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz.
+	Açıklama:
+		- Fibonacci serisi
+			0 1 1 2 3 5 8 13 ...
+		- Metot pozitif olmayan değerler için kontrol yapmayacaktır
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
 	{	
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int n = kb.nextInt();
-		
-		for (int i = 1; i <= n; ++i) {
-			if (i % 2 != 0)
-				continue;
-			
-			System.out.printf("%d ", i);
-		}
-		
-		System.out.println("\nTekrar yapıyor musunuz?");
+		FibaonnaciNumberTest.run();
 	}
 }
 
+
+class FibaonnaciNumberTest {
+	public static void run()
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		for (;;) {
+			System.out.print("Bir sayı giriniz:");
+			int n = Integer.parseInt(kb.nextLine());
+			
+			if (n < 1)
+				break;
+			System.out.printf("%d.Fibonacci sayısı:%d%n", n, NumberUtil.fibonacciNumber(n));
+		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
+	}
+}
+
+class NumberUtil {
+	public static int fibonacciNumber(int n)
+	{
+		//TODO:
+	}
+}
