355908ae35d440c2017bfbfd03f2380dd3a6e919
Oğuz Karan <oguzkaran@csystem.org>
2023-01-15 18:58:00 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,26 +1,46 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Parametresi ile aldığı int türden yıl değerinin artık yıl olup olmadığını test edebn isLeapYeaar
-	isimli metodu yazınız ve aşağıdaki kod ile test ediniz 
+	Sınıf Çalışması: Parametresi ile aldığı int türden gün ay ve yıl bilgisine ilişkin tarihin geçerli olup olmadığını
+	test eden isValidDate isimli metodu yazınız ve aşağıdaki kod ile test ediniz  
+	(İleride daha iyisi yazılacaktır)
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
 	{	
-		IsLeapYearTest.run();
+		IsValidDateTest.run();
 	}
 }
 
-class IsLeapYearTest {
+class IsValidDateTest {
 	public static void run()
 	{
-		for (int y = 1996; y <= 2104; ++y)
-			if (DateUtil.isLeapYear(y))
-				System.out.println(y);
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		for (;;) {
+			System.out.print("Gün ay ve yıl bilgilerini giriniz:");
+			int day = kb.nextInt();
+			int month = kb.nextInt();
+			int year = kb.nextInt();
+			
+			if (day == 0)
+				break;
+			
+			if (DateUtil.isValidDate(day, month, year))
+				System.out.printf("%02d/%02d/%04d tarihi geçerlidir%n", day, month, year);
+			else
+				System.out.println("Geçersiz tarih!...");
+			
+		}
 	}
 }
 
 class DateUtil {
+	public static boolean isValidDate(int day, int month, int year)
+	{
+		//TODO:
+	}
+	
 	public static boolean isLeapYear(int year)
 	{
 		return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
