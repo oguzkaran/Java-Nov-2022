15e03c9a05259aac37afa3666c66f445b6457186
Oğuz Karan <oguzkaran@csystem.org>
2023-01-21 20:37:18 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,15 +1,21 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	double türünden float türüne doğrudan atama geçersizdir
+	 İşlem Öncesi Otomatik Tür Dönüşümü:
+	 İki operandlı bir operqtör için derleyici önce operandların türüne bakar. Operandlar farklı türdensa bve işlem 
+	 geçerliyse, ortak bir türe dönüştürerek işlem yapılır. Hatta bazı durumlarda iki tür aynı olsa bile ikisini de 
+	 farklı bir türe dönüştürür. Yani derleyici buna yönelik kodlar üretir. Bu dönüştürmeye "işlem önce öncesi otomatik
+	 tür dönüşümü" denir
+	 
+	 
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)
 	{
-		double a = 12;
-		float b;
+		short a = 10;
+		short b = 20;
+		short c;
 		
-		b = a; //error 
-	
+		c = a + b;
 	}
 }
