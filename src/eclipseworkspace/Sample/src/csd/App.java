ee01968dcaaff1a0ea62a07bcbff8a682b97719b
Oğuz Karan <oguzkaran@csystem.org>
2023-02-01 01:46:03 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,7 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	 double türünden float türne yapılan explicit dönüşümde bilgi kaybı olabilir. Bu durumda IEEE 754 standardına göre 
-	 yuvarlama hatalaı oluşabilir ve değer float türü ile temsil edilebilecek en yakın değer olarak elde edilir. Ayrıca sayı 
-	 float türü sınırları dışındaysa da aynı şekilde yapılır 
+	  
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
