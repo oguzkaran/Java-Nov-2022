1705ce5ca120ba1453cb201bfd9d5f560bdfc6fd
Oğuz Karan <oguzkaran@csystem.org>
2023-02-05 22:08:45 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,46 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Homework-003-7. sorunun bir çözmü
-	(Not: Çözüm çalışma sorusunun verildiği tarihe kadar işlenmiş olan konulara göre yazılmıştır) 
+	Nesne Yönelimli Programlama Tekniği (NYPT): Bu tekniği tek bir cümle ilem anlatmak pek mümkün değildir. Ancak bu 
+	tekniği bilen birisine "sınıflar kullanarak program yazma tekniğidir" şeklinde bir cümle kurulabilir. Aslında 
+	Aslında NYPT anahtar kavramların birleşiminde oluşur. Bu anahta kavramlar birbirlerinden tamamen ayrık değildir.
+	Bu kavramlar içiçe geçmiş daireler gibi düşünülmelidir. Tüm bu anahtar kavramların temelinde kodun daha iyi
+	algılanabilir olması ve daha iyi yönetilmesi vardır. Örneğin benzer işi yapan metotlara aynı ismi vermek iyi bir
+	tekniktir. Çünkü bu durumda bu metotları kullanan kişiler "çok şey var" algısından uzaklaşıp "az şey var" gibi bir
+	algı oluştururlar. Bu da hatırlamayı kolaylaştırır  
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
-	{	
-		IsDecimalHarshadTest.run();
-	}
-}
-
-class IsDecimalHarshadTest {
-	public static void run()
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		System.out.print("Bir sayı giriniz:");
-		int n = kb.nextInt();
-		
-		while (n-- > 0)
-			if (NumberUtil.isDecimalHarshad(n + 1))
-				System.out.println(n + 1);	
-	}
-}
-
-class NumberUtil {
-	public static boolean isDecimalHarshad(int val)
-	{
-		return val > 0 && val % sumDigits(val) == 0; 
-	}
-	
-	public static int sumDigits(long a)
-	{
-		int sum = 0;
-		
-		while (a != 0) {
-			sum += a % 10;
-			a /= 10;
-		}
-		
-		return Math.abs(sum);
 	}
 }
 
