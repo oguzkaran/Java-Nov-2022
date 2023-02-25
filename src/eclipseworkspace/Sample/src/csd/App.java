986af93749d0bba9ec916f324071c786fc32eb5a
Oğuz Karan <oguzkaran@csystem.org>
2023-02-25 18:46:19 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,58 +1,41 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Bir metodun parametre değişkeni bir referans olabilir. Bu durumda metot aynı türden bir referans ile çağrılabilir. 
-	Metoda geçilen referans ile metot içerisinde ilgili nesneye erişilmiş olur. Referans parametreli bir metot aldığı
-	referansa ilişkin nesne üzerinde değişiklik yapabilir. Bu durumda bu değişiklik metodu çağrıdan koddaki referans
-	üzerinden aynı nesneye erişildiği için görülebilir. Bir metodun geri dönüş değeri de referans olabilir. Bu durumda
-	metodun geri dönüş değerine ilişkin return deyimine ait ifadenin türünün aynı türden bir referans olması gerekir.
-	
-	Aşağıdaki örnekte createTime metodu, içeride yaratmış olduğu nesnenin referansına geri dönmektedir. printTime
-	metodu parametresi ile aldığı Time türünden referansın gösterdiği nesneye erişip bilgilerini akrana yazdırmaktadır.
-	changeTime metodu aldığı refeeransa ilişkin nesneye erişip hour, minute, second ve millisecond elemanlarının 
-	değerlerini değiştirmektedir. Örneği çalıştırarak durumu gözlemleyiniz
+	Sınıfın non-static bir metodu içerisinde o sınıfın non-static metodu doğrudan çağrılabilir. Doğrudan çağrılan metot,
+	çağıran metodun çağrılmasında kullanılan referans ile çağrılmış olur
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
 	{
-		Time time;
+		Sample s1, s2;
 		
-		time = TimeUtil.createTime(19, 19, 14, 234);
-		TimeUtil.printTime(time);
-		TimeUtil.changeTime(time, 20, 34, 45, 345);
-		TimeUtil.printTime(time);
+		s1 = new Sample();
+		s2 = new Sample();
+		
+		s1.foo(20);
+		s2.foo(30);
+		
+		System.out.printf("s1.x = %d%n", s1.x);
+		System.out.printf("s2.x = %d%n", s2.x);
 	}
 }
 
-class TimeUtil {
-	public static Time createTime(int hour, int minute, int second, int millisecond)
-	{
-		Time time = new Time();
-		
-		changeTime(time, hour, minute, second, millisecond);
-		
-		return time;
-	}
-	
-	public static void changeTime(Time time, int hour, int minute, int second, int millisecond)
-	{
-		//...
-		time.hour = hour;
-		time.minute = minute;
-		time.second = second;
-		time.millisecond = millisecond;
-	}
-	
-	public static void printTime(Time time)
-	{
-		System.out.printf("%02d:%02d:%02d.%03d%n", time.hour, time.minute, time.second, time.millisecond);
-	}
-}
 
-class Time {
-	public int hour;
-	public int minute;
-	public int second;
-	public int millisecond;
+class Sample {
+	public int x;
+	
+	public void foo(int a)
+	{
+		bar(a);
+	}
+	
+	public void bar(int a)
+	{
+		x = a;
+	}
+	
 	//...
 }
+
+
+
