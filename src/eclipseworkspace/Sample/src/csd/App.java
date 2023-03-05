be86b1c428e7ecc5e7e444f6412e949057077403
Oğuz Karan <oguzkaran@csystem.org>
2023-03-05 18:38:39 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,23 +1,5 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	ctor aşağıdaki özelliklere sahp bir metottur:
-	- Sınıfı yazan programcı, sınıfa hiç bir ctor yazmazsa, derleyici parametresiz ctor'u (default ctor) public ve
-	içi boş olarak otomatik olarak yazar
-	
-	- Sınıfı yazan programcı en az bir tane ctor yazarsa, default ctor derleyici tarafından yazılmaz. Artık default 
-	ctor'un yazılmayacağına programcı karar verecektir
-	
-	- ctor overload edilebilir (constructor overloading)
-	
-	- Sınıfın ctor'u sınıf ismi ile aynı olan ve geri dönüş değeri kavramı olmayan bir metottur. Geri dönüş değeri
-	bilgisi yazılmaz. void da yazılmaz. Sınıf ismi ile aynı isimde bir metot yazılıp geri dönüş değeri bilgisi yazılırsa
-	artık bu metot ctor olmaz. Şüphesiz böyle bir metot yazılmamalıdır
-	
-	- ctor non-static bir metottur
-	
-	- ctor new operatörüğ ile nesne yaratılmasın sırasında çağrılır. Hangi ctor'un çağrılacağı derleme zamanında 
-	new operatörü ile kullanımda geçilen argümanlara göre "method overload resolution" kuralları ile belirlenir
-	
-	- 
+	Point sınıfı
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
@@ -28,19 +10,3 @@ class App {
 	}
 }
 
-class Sample {
-	public Sample(int a)
-	{
-		
-	}
-	
-	public Sample(double a)
-	{
-		
-	}
-	
-	public void foo()
-	{
-		
-	}
-}
