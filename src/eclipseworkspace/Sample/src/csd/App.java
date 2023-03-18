fe87aa1774ace01e5f2ed1f1f8f0210f87856df5
Oğuz Karan <oguzkaran@csystem.org>
2023-03-18 18:32:12 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,12 +1,27 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	
------------------------------------------------------------------------------------------------------------------------*/
+  	Character toXXX metotları. Aşağıdaki örneği inceleyiniz
+---------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
 	{	
+		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
+		System.out.print("Bir yazı giriniz:");
+		String str = kb.nextLine();
+		
+		int len = str.length();
+		
+		for (int i = 0; i < len; ++i) {
+			char ch = str.charAt(i);
+			
+			ch = Character.isLowerCase(ch) ? Character.toUpperCase(ch) : Character.toLowerCase(ch);
+			
+			System.out.printf("%s", ch);
+		}
+		
+		System.out.println();
 	}
 }
 
