68b6ff52dbbaaa2679b9b21d0b789c3cfa748340
Oğuz Karan <oguzkaran@csystem.org>
2023-03-19 20:13:06 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,59 +1,133 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Paramatresi ile aldığı int türden bir n değeri için n tane rasgele üretilmiş Türkçe
-	karakterden oluşan bir yazı döndüren getRandomtextTR ile n tane rasgele üretilmiş İngilizce karakterden oluşan
-	bir yazı döndüren getRandomTextEN metotlarını yazınız ve aşağıdaki kod ile test ediniz
+	
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
 	{	
-		GetRandomTextTRENTest.run();
+		Complex z1 = new Complex(3.4, 9.7);
+		Complex z2 = new Complex();
+		Complex z3 = new Complex(3.4);
+		
+		System.out.println(z1.toString());
+		System.out.println(z2.toString());
+		System.out.println(z3.toString());
 	}
 }
 
-class GetRandomTextTRENTest {
-	public static void run()
+class Complex {
+	public double real;
+	public double imag;
+	
+	public static Complex add(double re1, double im1, double re2, double im2)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		java.util.Random r = new java.util.Random();
-		
-		for (;;) {
-			System.out.print("Bir sayı giriniz:");
-			int n = Integer.parseInt(kb.nextLine());
-			
-			if (n <= 0))
-				break;
-			
-			System.out.printf("Şifre:%s%n", StringUtil.getRandomTextTR(r, n));
-			System.out.printf("Password:%s%n", StringUtil.getRandomTextEN(r, n));
-		}
+		return new Complex(re1 + re2, im1 + im2);
 	}
+	
+	public static Complex subtract(double re1, double im1, double re2, double im2)
+	{
+		return add(re1, im1, -re2, -im2);
+	}
+	
+	public static Complex multiply(double re1, double im1, double re2, double im2)
+	{
+		//TODO:
+		return new Complex();
+	}
+	
+	public Complex()
+	{
+	}
+	
+	public Complex(double re)
+	{
+		real = re;
+	}
+	
+	public Complex(double re, double im)
+	{
+		real = re;
+		imag = im;
+	}
+	
+	public static Complex add(double val, Complex z)
+	{
+		return add(val, 0, z.real, z.imag);
+	}
+	
+	public Complex add(double re, double im)
+	{
+		return add(real, imag, re, im);
+	}
+	
+	public Complex add(double val)
+	{
+		return add(real, imag, val, 0);
+	}
+	
+	public Complex add(Complex other)
+	{
+		return add(other.real, other.imag);
+	}
+	
+	public static Complex subtract(double val, Complex z)
+	{
+		return subtract(val, 0, z.real, z.imag);
+	}
+	
+	public Complex subtract(double re, double im)
+	{
+		return subtract(real, imag, re, im);
+	}
+	
+	public Complex subtract(double val)
+	{
+		return subtract(real, imag, val, 0);
+	}
+	
+	public Complex subtract(Complex other)
+	{
+		return subtract(other.real, other.imag);
+	}
+	
+	public static Complex multiply(double val, Complex z)
+	{
+		return multiply(val, 0, z.real, z.imag);
+	}
+	
+	public Complex multiply(double re, double im)
+	{
+		return multiply(real, imag, re, im);
+	}
+	
+	public Complex multiply(double val)
+	{
+		return multiply(real, imag, val, 0);
+	}
+	
+	public Complex multiply(Complex other)
+	{
+		return multiply(other.real, other.imag);
+	}
+	
+	public Complex getConjugate()
+	{
+		return new Complex(real, -imag);
+	}
+	
+	public double getLength()
+	{
+		return Math.sqrt(real * real + imag * imag);
+	}
+	
+	public double getNorm()
+	{
+		return getLength();
+	}
+	
+	public String toString()
+	{
+		return String.format("(%.2f, %.2f)", real, imag);
+	}	
 }
-
-
-class StringUtil {
-	public static String getRandomTextTR(java.util.Random r, int n)
-	{
-		
-	}
-	
-	public static String getRandomTextTR(int n)
-	{
-		return getRandomTextTR(new java.util.Random(), n);
-	}
-	
-	 
-	
-	public static String getRandomTextEN(java.util.Random r, int n)
-	{
-		
-	}
-	
-	public static String getRandomTextEN(int n)
-	{
-		return getRandomTextEN(new java.util.Random(), n);
-	}
-}
-
-
