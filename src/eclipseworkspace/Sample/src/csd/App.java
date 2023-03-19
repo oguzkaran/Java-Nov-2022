a5f85c526eab78d39e6d20f90a00bcbfb6f84023
Oğuz Karan <oguzkaran@csystem.org>
2023-03-19 18:14:10 +0300

    Update

--- src/eclipseworkspace/Sample/src/csd/App.java
+++ src/eclipseworkspace/Sample/src/csd/App.java
@@ -1,81 +1,75 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: Aşağıda açıklanan metotları StringUtil içerisinde yazınız ve aşağıdaki kod ile test ediniz.
-		public static String padLeading(String s, int len, char ch);
-		public static String padLeading(String s, int len);
-		public static String padTrailing(String s, int len, char ch);
-		public static String padTrailing(String s, int len);
-		
-	Açıklamalar:
-		- padLeading metodunun 3 parametreli overload'u ikinci parametresi ile aldığı len, birinci parametresi ile 
-		aldığı yazının uzunluğundan büyükse yazıyı soldan (baştan) üçüncü parametresi ile aldığı karakter ile
-		doldurulmuş yeni bir String referansına geri dönecektir.
-		Örneğin:
-			padLeading("ankara", 12, 'x'); çağrısı
-				xxxxxxankara
-			yazısını döndürecektir
-			
-		- len uzunluğu yazının uzunluğundan kçük veya eşitse aynı referanda geri dönecektir
-		
-		- padLeading metodunun 2 parametreli overload'u yazıyı duruma göre soldan (baştan) space karakteri ile
-		dolduracaktır
-		
-		- padTrailing metotları benzer şekilde ancak sağdan (sondan) dolduracak şekilde yazılacaktır
-		
-		- Metotları Java 11+ ve Java 11 öncesi için ayrı ayrı yazınız
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının Türkçe pangram olup olmadığını test eden isPangramTR
+	ve İngilizce pangram olup olmadığını test eden isPangramEN metotlarını StringUtil sınıfı içerisinde yazınız
+	ve aşağıdaki kod ile test ediniz.
+	
+	Pangram: İçerisinde özel isim olmayan ve ilgili dilin alfabesindeki tüm karakterleri içeren anlamlı cümlelere denir
+	
+	Parogramda özel isim ve anlamlı olması kontrolü yapılmayacaktır
+	
+	İngilizce: The quick brown fox jumps over the lazy dog
+	Türkçe: Pijamalı hasta yağız şoföre çabucak güvendi
 -----------------------------------------------------------------------------------------------------------------------*/
 package csd;
 
 class App {
 	public static void main(String [] args)	
 	{	
-		PadLeadingTrailingTest.run();
+		IsPangramTest.run();
 	}
 }
 
-class PadLeadingTrailingTest {
+class IsPangramTest {
 	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		String str = "";
 		
-		for (;;) {
-			System.out.print("Bir yazı giriniz:");
-			String s = kb.nextLine();
-		
-			if ("elma".equals(s))
-				break;
-			
-			System.out.print("Bir sayı giriniz:");
-			int count = Integer.parseInt(kb.nextLine());
-			
-			System.out.printf("(%s)%n", StringUtil.padLeading(str, count, 'x'));
-			System.out.printf("(%s)%n", StringUtil.padLeading(str, count));
-			System.out.printf("(%s)%n", StringUtil.padTrailing(str, count, 'x'));
-			System.out.printf("(%s)%n", StringUtil.padTrailing(str, count));
-		}
-		
-		System.out.println("Tekrar yapıyor musunuz?");
+		IsPangramTRTest.run(kb);
+		IsPangramENTest.run(kb);
 	}
 }
 
+class IsPangramENTest {
+	public static void run(java.util.Scanner kb)
+	{
+		for (;;) {
+			System.out.print("Input a text:");
+			String s = kb.nextLine();
+			
+			if ("quit".equals(s))
+				break;
+			
+			System.out.println(StringUtil.isPangramEN(s) ? "Pangram" : "No a Pangram");
+		}
+	}
+}
+
+class IsPangramTRTest {
+	public static void run(java.util.Scanner kb)
+	{
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();
+			
+			if ("elma".equals(s))
+				break;
+			
+			System.out.println(StringUtil.isPangramTR(s) ? "Pangram" : "Pangram değil");
+		}
+	}
+}
+
+
 class StringUtil {
-	public static String padLeading(String s, int len, char ch)
+	public static boolean isPangramTR(String s)
 	{
 		//TODO:
 	}
 	
-	public static String padLeading(String s, int len)
-	{
-		//TODO:
-	}
-	
-	public static String padTrailing(String s, int len, char ch)
-	{
-		//TODO:
-	}
-	
-	public static String padTrailing(String s, int len)
+	public static boolean isPangramEN(String s)
 	{
 		//TODO:
 	}
 }
+
+
