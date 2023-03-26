fb8655d219dfd149a376ed9f1711af0c95621673
Oğuz Karan <oguzkaran@csystem.org>
2023-03-26 19:08:36 +0300

    Update

--- src/eclipseworkspace/Sample/src/org/csystem/app/App.java
+++ src/eclipseworkspace/Sample/src/org/csystem/app/App.java
@@ -1,13 +1,24 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Niteliksiz isim arama genel kuralları (else if biçiminde değerlendiriniz):
-	4. "import on demand declaration" varsa o pakete de bakılır. Bu konu ileride ele alınacaktır  
+	Nitelikli isim arama genel kuralları (else if biçiminde değerlendiriniz):
+	1. Aranacak ismin solunda bir UDT ismi varsa, ilgili UDT içerisinde aranır. 
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String [] args)
 	{	
-		Sample s;
+		Sample.x = 10;
+		Sample.foo();
+		
+		System.out.printf("Sample.x = %d%n", Sample.x);
+	}
+}
+
+class Sample {
+	public static int x;
+	public static void foo()
+	{
+		System.out.println("Sample.foo");
 	}
 }
 
