7da4403bdcbcbe89b2ecbea89ade5ba04391ea40
Oğuz Karan <oguzkaran@csystem.org>
2023-04-02 18:04:23 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,18 +1,35 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Berk Tunç
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir dizinin elemanlarının toplamına geri dönen sum isimli
+	metodu ArrayUtil sınıfı içerisinde yazınız ve aşağıdaki kod ile test ediniz
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import org.csystem.util.array.ArrayUtil;
+import java.util.Random;
+import java.util.Scanner;
+
+import static org.csystem.util.array.ArrayUtil.getRandomArray;
+import static org.csystem.util.array.ArrayUtil.print;
 
 class App {
 	public static void main(String [] args)
 	{
-		int [] a = {1, 2, 3, 4, 5};
+		Scanner kb = new Scanner(System.in);
+		Random random = new Random();
 
-		ArrayUtil.print(a);
-		ArrayUtil.swap(a, 1, 3);
-		ArrayUtil.print(a);
+		for (;;) {
+			System.out.print("Bir sayı giriniz:");
+			int count = Integer.parseInt(kb.nextLine());
+
+			if (count <= 0)
+				break;
+			int[] a = getRandomArray(random, count, 0, 99);
+
+			System.out.println("--------------------------------------------------------------------------------------");
+			print(a);
+			System.out.println("--------------------------------------------------------------------------------------");
+		}
+
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
--- src/Sample/Sample/src/org/csystem/util/array/ArrayUtil.java
+++ src/Sample/Sample/src/org/csystem/util/array/ArrayUtil.java
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------
 	FILE			: ArrayUtil.java
 	AUTHOR			: Java-Nov-2022 Group
-	LAST UPDATE		: 01.04.2023
+	LAST UPDATE		: 02.04.2023
 
 	Utility class for array operations
 
@@ -10,13 +10,36 @@
 ------------------------------------------------------------*/
 package org.csystem.util.array;
 
+import java.util.Random;
+
 public class ArrayUtil {
+
+    public static void fillRandomArray(Random random, int [] a, int min, int bound)
+    {
+        for (int i = 0; i < a.length; ++i)
+            a[i] = random.nextInt(min, bound);
+    }
+
+    public static int [] getRandomArray(Random random, int count, int min, int bound)
+    {
+        int [] a = new int[count];
+
+        fillRandomArray(random, a, min, bound);
+
+        return a;
+    }
+
     public static void print(int [] a)
     {
         for (int i = 0; i < a.length; ++i)
             System.out.printf("%d ", a[i]);
 
         System.out.println();
+    }
+
+    public static int sum(int [] a)
+    {
+        //TODO:
     }
 
     public static void swap(int [] a, int i, int k)
