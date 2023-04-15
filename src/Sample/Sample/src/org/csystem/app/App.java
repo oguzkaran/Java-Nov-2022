d11d23003f995e4c7bbc354d89dea652a292b5f1
Oğuz Karan <oguzkaran@csystem.org>
2023-04-15 21:04:50 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,16 +1,19 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıf Çalışması: 6 tane [1, 49] aralığın tekrarlanmayan sayılardan oluşan bir sayısal loto kuponu üreten programı
-	yazınız
-	Not: İleride daha iyisi yazılacaktır
+	Sınıf Çalışması: Aşağıdaki prototipi ve açıklması verilen metodu ArrayUtil sınıfı içerisinde yazınız:
+		int [] getHistogramData(int [] a, n)
+	Açıklamalar:
+		- Metodun aldığı dizi içerisinde [0, n] aralığında sayılar olacaktır
+		- Metot [0, n] aralığındaki sayıların sıklık sayısını içeren diziye geri dönecektir. Yani her bir elamandan
+		kaç tane olduğunu tutan bir diziye dönecektir. Geri döndüğü dizinin sıfır numaralı indekteki elemanı sıfır sayısının,
+		1 numaralı indekteki elemanı 1 sayısının, ...,  n numaralı indekteki elemanı n sayısının kaç tane olduğu bilgisini
+		içerecektir
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
-
-import org.csystem.app.lottery.numericlottery.NumericLotteryApp;
 
 class App {
 	public static void main(String [] args)
 	{
-		NumericLotteryApp.run();
+
 	}
 }
 
--- src/Sample/Sample/src/org/csystem/random/lottery/numericlottery/NumericLottery.java
+++ src/Sample/Sample/src/org/csystem/random/lottery/numericlottery/NumericLottery.java
@@ -1,11 +1,39 @@
 package org.csystem.random.lottery.numericlottery;
 
-import org.csystem.util.array.ArrayUtil;
-
 import java.util.Random;
 
 public class NumericLottery {
     public Random random;
+
+    public static int [] getNumbers(boolean [] flags)
+    {
+        int [] numbers = new int[6];
+        int index = 0;
+
+        for (int i = 1; i < 50; ++i)
+            if (flags[i])
+                numbers[index++] = i;
+
+        return numbers;
+    }
+
+    public boolean [] getFlags()
+    {
+        boolean [] flags = new boolean[50];
+
+        for (int i = 0; i < 6; ++i) {
+            int val;
+
+            for (;;) {
+                val = random.nextInt(1, 50);
+                if (!flags[val])
+                    break;
+            }
+            flags[val] = true;
+        }
+
+        return flags;
+    }
 
     public NumericLottery()
     {
@@ -19,25 +47,6 @@ public class NumericLottery {
 
     public int [] getNumbers()
     {
-        int [] numbers = new int[6];
-
-        for (int i = 0; i  < 6; ++i) {
-            boolean repeat;
-
-            do {
-                repeat = false;
-                numbers[i] = random.nextInt(1, 50);
-
-                for (int k = 0; k < i; ++k)
-                    if (numbers[i] == numbers[k]) {
-                        repeat = true;
-                        break;
-                    }
-            } while (repeat);
-        }
-
-        ArrayUtil.bubbleSort(numbers);
-
-        return numbers;
+        return getNumbers(getFlags());
     }
 }
