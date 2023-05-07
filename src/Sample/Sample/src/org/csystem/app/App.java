422826f68c2f90aac17d125fe81c55fa94b28d11
Oğuz Karan <oguzkaran@csystem.org>
2023-05-07 20:27:18 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,5 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıfın public bölümüne sınıf dışından erişilebilir. Aşağıdaki örnekte sınıfların aynı pakette olduğuna dikkat ediniz
+	Sınıfların özellikle veri elemanlarının gizlenmesine yani diğer sınıflardan erişiminin engellenmesine nesne yönelimli
+	programlama tekniğinde "kapsülleme/sınıfsal temsil (encapsulation)" denir. Bu kavramın geneline de
+	"veri/bilgi gizleme (data/information hiding)" denir. Burada temel amaç dışarıyı yani sınıf dışını ilgilendirmeyecek
+	verilerin/bilgilerin dışarıya doğrudan verilmemesidir. Bu kavram, diğer birçok kavramda olduğu gibi gerçek hayattan
+	programlamaya aktarılmıştır. Örneğin, bir kişi muhtardan ikametgah senedi almak isterse, muhtarlık kanunu bilmesi
+	gerekmez. Ya da araba kullanırken vites değişiminin aşağı seviyede nasıl yapıldığı kullanan için önemsizdir. Bilse de
+	bilmese de arabayı kullanabilir.
+
+	Bu anlamda sınıfı yazan (server) ve sınıfı kullanan (client) bakış açıları farklıdır. Şüphesiz sınıfı yazan için
+	iki bakış açısı da önemlidir. Bu durumda programcı buna göre düşünerek sınıfı yazar. Sınıfı kullanan bakış açısı
+	için (müşteri kodları) içsel detayların önemi yoktur
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -9,4 +19,3 @@ class App {
 
 	}
 }
-
