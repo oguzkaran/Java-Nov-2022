0e9d554da09e10d1b80033be2e4311d64adbdedf
Oğuz Karan <oguzkaran@csystem.org>
2023-05-27 18:26:13 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,5 +1,15 @@
 /*----------------------------------------------------------------------------------------------------------------------
+	Sınıf Çalışması: Aşağıda açıklanan sınıfları yazınız
+	Açıklamalar:
+		- Sınıfların ismi IntValue ve LongValue olacaktır
+		- Sınıflar immutable olarak yazılacaktır
+		- IntValue ve LongValue sınıfları sırasıyla int türden ve long türden değerleri sarmalayacaktır (wrapper). Yani
+		int türden bir değeri tutacaktır
+		- Sınıflar [-128, 127] aralığındaki değerlere ilişkin sarmalaman nesneleri ilk kez istendiğinde yaratacak sonraki
+		isteklerde aynı nesnenin referansını verecektir. Yani bir "önbellek (cache)" tutacaktır.
 
+	Not: Java'da bu şekilde işlem yapan sınıflar vardır ve çok sık kullanılır. Burada böyle bir sınıfın nnasıl yazılması
+	gerektiğine odaklanınız
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
