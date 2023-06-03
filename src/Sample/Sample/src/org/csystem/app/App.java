1f3da952f2fcf795baef4fd88fa9ebb9e2538850
Oğuz Karan <oguzkaran@csystem.org>
2023-06-03 21:01:34 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,13 +1,27 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Anahtar Notlar: Java 8'den önce sınıfların factory metotları genel olarak valueOf biçiminde isimlendirilmiştir. Java 8
-	ile birlikte bu tarz metotların ofXXX olarak isimlendirilmiştir
+	Sınıflararası İlişkiler:
+	
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Scanner;
 
 class App {
 	public static void main(String [] args)
 	{
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Input the device status as OPEN, CLOSED or UNKNOWN:");
+		String deviceStatusStr = kb.nextLine().toUpperCase();
+		DeviceStatus deviceStatus = DeviceStatus.valueOf(deviceStatusStr);
 
+		switch (deviceStatus) {
+			case OPEN -> System.out.println("Open device");
+			case CLOSED -> System.out.println("Closed device");
+			case UNKNOWN -> System.out.println("Unknown device");
+		}
 	}
 }
 
+enum DeviceStatus {
+	OPEN, CLOSED, UNKNOWN
+}
