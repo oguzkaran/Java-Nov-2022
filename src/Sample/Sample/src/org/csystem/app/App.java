9ddd321593d220f839dae1ad0a280404dd415686
Oğuz Karan <oguzkaran@csystem.org>
2023-07-07 19:39:02 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,17 +1,18 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	ArrayList sınıfının toString metodu ile ArrayList'in elemanları [] içeerisinde virgülle ayrılacak şekilde bir String
-	elde edilebilir. Bu String'in elde edilme detayları ileride ele alınacaktır
+	Sınıf Çalışması: Klavyeden elma girilene kadar alınan yazıları bir ArrayList içerisinde tutunuz. Bu işlemden sonra
+	klavyeden alınan bir yazının ArrayList'in tüm elemanlarının arasında olacak şekilde birleştirildiği bir String
+	elde eden programı yazınız
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Vector;
 
 class App {
 	public static void main(String [] args)
 	{
 		Scanner kb = new Scanner(System.in);
-		ArrayList names = new ArrayList();
+		Vector names = new Vector();
 
 		while (true) {
 			System.out.print("İsim giriniz:");
@@ -23,6 +24,12 @@ class App {
 			names.add(name);
 		}
 
-		System.out.println(names.isEmpty() ? "Hiç isim girmediniz!..." : names.toString());
+		System.out.printf("Capacity:%d%n", names.capacity());
+		names.ensureCapacity((int)Math.floor(names.capacity() / 2.));
+		System.out.printf("Capacity:%d%n", names.capacity());
+		names.ensureCapacity((int)Math.floor(names.capacity() * 1.5));
+		System.out.printf("Capacity:%d%n", names.capacity());
+		names.ensureCapacity(names.capacity() * 2 + 1);
+		System.out.printf("Capacity:%d%n", names.capacity());
 	}
 }
