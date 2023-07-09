346135acb0a4daf573a07981d406f3602f84af6a
Oğuz Karan <oguzkaran@csystem.org>
2023-07-09 20:44:36 +0300

    Update

--- src/Sample/Sample/src/org/csystem/app/App.java
+++ src/Sample/Sample/src/org/csystem/app/App.java
@@ -1,5 +1,8 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Arayüzler (Interfaces):
+	abstract sınıflar ile arayüzler arasındaki farklar:
+	-
+
+	Soyut bir tür ne zaman abstract sınıf yapılmalıdır? Programcı buna nasıl karar verecektir?
 -----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -7,5 +10,38 @@ class App {
 	public static void main(String [] args)
 	{
 
+	}
+}
+class A implements IX, IY, IZ {
+	public void foo()
+	{
+		System.out.println("A.foo");
+		super.foo(); //error
+	}
+
+	public void bar()
+	{
+		System.out.println("A.bar");
+	}
+
+	public void tar()
+	{
+		System.out.println("A.tar");
+	}
+}
+
+interface IX {
+	void foo();
+}
+
+interface IY {
+	void bar();
+}
+
+interface IZ {
+	void tar();
+	default void foo()
+	{
+		System.out.println("IZ.foo");
 	}
 }
