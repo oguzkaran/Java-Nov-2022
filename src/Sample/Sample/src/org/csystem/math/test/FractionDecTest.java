fcc005d808a8ec4ad7fa49824dd8909036a1930f
Oğuz Karan <oguzkaran@csystem.org>
2023-07-24 09:43:32 +0300

    Update

---
+++ src/Sample/Sample/src/org/csystem/math/test/FractionDecTest.java
@@ -0,0 +1,35 @@
+package org.csystem.math.test;
+
+import org.csystem.math.Fraction;
+import org.csystem.math.test.factory.FractionFactory;
+import org.csystem.util.console.Console;
+
+public class FractionDecTest {
+    private static void run()
+    {
+        FractionFactory factory = new FractionFactory();
+
+        int count = Console.readInt("Bir sayı giriniz: ");
+
+        while (count-- > 0) {
+            try {
+                Fraction f = factory.createRandomFraction(-10, 10);
+                System.out.println("--------------------------------------------------------------");
+                Console.writeLine("f = %s", f);
+                f.dec();
+                Console.writeLine("f - 1 = %s", f);
+                System.out.println("--------------------------------------------------------------");
+            }
+            catch (NumberFormatException ignore)
+            {
+                Console.writeLine("Invalid Fraction Value");
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        run();
+    }
+}
+
+
