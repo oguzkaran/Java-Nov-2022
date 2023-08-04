9a7041951f015d3f3a9ff25e33dfd5284612eb1a
Oğuz Karan <oguzkaran@csystem.org>
2023-08-04 20:11:25 +0300

    Update

--- src/Sample/Sample/src/com/bartucankaya/test/tuple/PairEqualsTest.java
+++ src/Sample/Sample/src/com/bartucankaya/test/tuple/PairEqualsTest.java
@@ -2,47 +2,17 @@ package com.bartucankaya.test.tuple;
 
 import org.csystem.collection.tuple.Pair;
 import org.csystem.math.geometry.Point;
-import org.csystem.util.collection.CollectionUtil;
 import org.csystem.util.console.Console;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.random.RandomGenerator;
-
 public class PairEqualsTest {
-    public static void run()
-    {
-        RandomGenerator random = new Random();
-        ArrayList<Pair<Integer, Point>> points = new ArrayList<>();
-
+    public static void run() {
         Point origin = Point.ofCartesian();
-        int i = 1;
-        while (true) {
-            Point p = random.nextBoolean() ? Point.ofCartesian() : Point.ofCartesian(random.nextDouble(-100, 100), random.nextDouble(-100, 100));
 
-            points.add(new Pair<>(i++, p));
-            if (p.equals(origin))
-                break;
-        }
+        Pair<Integer, Point> pair1 = new Pair<Integer, Point> (1, origin);
+        Pair<Integer, Point> pair2 = new Pair<Integer, Point> (1, Point.ofCartesian(0, 0));
 
-        Console.writeLine("Points:");
-        CollectionUtil.print(points, "\n", "");
-
-        Console.writeLine("Copying the list...");
-        ArrayList points2 = (ArrayList) points.clone();
-
-        CollectionUtil.print(points2, "\n", "");
-        Console.writeLine("--------------------------------------------------------------");
-        Console.writeLine(points.equals(points2) ? "Same list" : "Different lists");
-
-        Console.writeLine("Removing the last pair of the list...");
-
-        if (!points2.isEmpty())
-            points2.remove(points2.size() - 1);
-
-        CollectionUtil.print(points2, "\n", "");
-        Console.writeLine("--------------------------------------------------------------");
-        Console.writeLine(points.equals(points2) ? "Same list" : "Different lists");
+        Console.writeLine("Pair 1: %s%nPair 2: %s", pair1.toString(), pair2.toString());
+        Console.writeLine(pair1.equals(pair2) ? "Same pair" : "Different pair");
     }
 
     public static void main(String[] args)
--- src/Sample/Sample/src/com/bartucankaya/test/tuple/TripleEqualsTest.java
+++ src/Sample/Sample/src/com/bartucankaya/test/tuple/TripleEqualsTest.java
@@ -3,48 +3,18 @@ package com.bartucankaya.test.tuple;
 import org.csystem.collection.tuple.Triple;
 import org.csystem.math.geometry.Circle;
 import org.csystem.math.geometry.Point;
-import org.csystem.util.collection.CollectionUtil;
 import org.csystem.util.console.Console;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.random.RandomGenerator;
-
 public class TripleEqualsTest {
-    public static void run()
-    {
-        RandomGenerator random = new Random();
-        ArrayList<Triple<Integer, Point, Circle>> points = new ArrayList<>();
-
+    public static void run() {
         Point origin = Point.ofCartesian();
-        int i = 1;
-        while (true) {
-            Point p = random.nextBoolean() ? Point.ofCartesian() : Point.ofCartesian(random.nextDouble(-100, 100), random.nextDouble(-100, 100));
-            Circle c = new Circle(random.nextDouble(0, 11));
+        Circle unit = new Circle(1);
 
-            points.add(new Triple<>(i++, p, c));
-            if (p.equals(origin))
-                break;
-        }
+        Triple<Integer, Point, Circle> triple1 = new Triple (1, origin, unit);
+        Triple<Integer, Point, Circle> triple2 = new Triple (1, Point.ofCartesian(0, 0), new Circle(1));
 
-        Console.writeLine("Points & Circles:");
-        CollectionUtil.print(points, "\n", "");
-
-        Console.writeLine("Copying the list...");
-        ArrayList points2 = (ArrayList) points.clone();
-
-        CollectionUtil.print(points2, "\n", "");
-        Console.writeLine("--------------------------------------------------------------");
-        Console.writeLine(points.equals(points2) ? "Same list" : "Different lists");
-
-        Console.writeLine("Removing the last triple of the list...");
-
-        if (!points2.isEmpty())
-            points2.remove(points2.size() - 1);
-
-        CollectionUtil.print(points2, "\n", "");
-        Console.writeLine("--------------------------------------------------------------");
-        Console.writeLine(points.equals(points2) ? "Same list" : "Different lists");
+        Console.writeLine("Triple 1: %s%nTriple 2: %s", triple1.toString(), triple2.toString());
+        Console.writeLine(triple1.equals(triple2) ? "Same triple" : "Different triple");
     }
 
     public static void main(String[] args)
