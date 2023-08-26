5843c4045a338e8ca1af5426d014547ed59693d9
Oğuz Karan <oguzkaran@csystem.org>
2023-08-26 20:15:36 +0300

    Update Application.java

--- src/Sample/Sample/src/org/csystem/app/Application.java
+++ src/Sample/Sample/src/org/csystem/app/Application.java
@@ -2,19 +2,39 @@ package org.csystem.app;
 
 import org.csystem.util.console.Console;
 
+import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+
+import static org.csystem.util.console.commandline.CommandLineArgsUtil.checkLengthEquals;
+
 public class Application {
     public static void run(String[] args)
     {
-        Console.writeLine(System.getProperty("user.dir"));
-        Console.writeLine(System.getProperty("user.home"));
-        Console.writeLine(System.getProperty("user.name"));
-        Console.writeLine(System.getProperty("os.arch"));
-        Console.writeLine(System.getProperty("os.name"));
-        Console.writeLine(System.getProperty("os.version"));
-        Console.writeLine(System.getProperty("java.version"));
-        Console.writeLine(System.getProperty("java.vendor"));
-        Console.writeLine(System.getProperty("java.vendor.url"));
-        Console.writeLine(System.getProperty("line.separator").equals("\r\n") ? "Windows" : "Not windows");
+        checkLengthEquals(args.length, 1, "Wrong number of arguments!...");
+
+        try {
+            Path path = Path.of(args[0]);
+
+            if (Files.exists(path))
+                if (Files.isDirectory(path))
+                    Console.writeLine("%s %s", path.getFileName(), Files.deleteIfExists(path) ? "deleted" : "not deleted");
+                else
+                    Console.writeLine("'%s' is not a directory!...", path.getFileName());
+            else
+                Console.writeErrLine("'%s' not exists!...", path.getFileName());
+        }
+        catch (DirectoryNotEmptyException ignore) {
+            Console.writeErrLine("Directory not empty!...");
+        }
+        catch (IOException ignore) {
+            Console.writeErrLine("I/O problem occurs!...");
+        }
+        catch (InvalidPathException ex) {
+            Console.writeErrLine("'%s' is invalid", ex.getInput());
+        }
     }
 
     public static void main(String[] args)
